e64ede701b585cb057bf95ff9f520bd5dd04dfb3
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-02-16 09:40:19 +0000

    DCAC-76: Add request DTO.

--- src/main/java/uk/gov/companieshouse/documentsigningapi/controller/SignDocumentController.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/controller/SignDocumentController.java
@@ -2,7 +2,9 @@ package uk.gov.companieshouse.documentsigningapi.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.companieshouse.documentsigningapi.dto.SignPdfRequestDTO;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -13,7 +15,11 @@ public class SignDocumentController {
             "${uk.gov.companieshouse.documentsigningapi.signpdf}";
 
     @PostMapping(SIGN_PDF_URI)
-    public ResponseEntity<Object> signPdf() {
+    public ResponseEntity<Object> signPdf(final @RequestBody SignPdfRequestDTO signPdfRequestDTO) {
+
+        // TODO DCAC-76 Use structured logging
+        System.out.println("signPdf(" + signPdfRequestDTO + ")");
+
         return ResponseEntity.status(OK).build();
     }
 
