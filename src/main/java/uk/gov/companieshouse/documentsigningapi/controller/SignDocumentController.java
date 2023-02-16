05f725786f8f545fc3af60b616d1658c95b82615
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-02-16 11:36:20 +0000

    DCAC-76: Use structured logging.

--- src/main/java/uk/gov/companieshouse/documentsigningapi/controller/SignDocumentController.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/controller/SignDocumentController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.documentsigningapi.dto.SignPdfRequestDTO;
 import uk.gov.companieshouse.documentsigningapi.dto.SignPdfResponseDTO;
+import uk.gov.companieshouse.documentsigningapi.logging.LoggingUtils;
+
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static uk.gov.companieshouse.documentsigningapi.logging.LoggingUtils.SIGN_PDF_REQUEST;
+import static uk.gov.companieshouse.documentsigningapi.logging.LoggingUtils.SIGN_PDF_RESPONSE;
 
 @RestController
 public class SignDocumentController {
@@ -15,16 +20,24 @@ public class SignDocumentController {
     public static final String SIGN_PDF_URI =
             "${uk.gov.companieshouse.documentsigningapi.signpdf}";
 
+    private final LoggingUtils logger;
+
+    public SignDocumentController(LoggingUtils logger) {
+        this.logger = logger;
+    }
+
     @PostMapping(SIGN_PDF_URI)
     public ResponseEntity<Object> signPdf(final @RequestBody SignPdfRequestDTO signPdfRequestDTO) {
 
-        final SignPdfResponseDTO response = new SignPdfResponseDTO();
-        response.setSignedDocumentLocation("there");
+        final SignPdfResponseDTO signPdfResponseDTO = new SignPdfResponseDTO();
+        signPdfResponseDTO.setSignedDocumentLocation("there");
 
-        // TODO DCAC-76 Use structured logging
-        System.out.println("signPdf(" + signPdfRequestDTO + ") returning " + response + ")");
+        final Map<String, Object> map = logger.createLogMap();
+        map.put(SIGN_PDF_REQUEST, signPdfRequestDTO);
+        map.put(SIGN_PDF_RESPONSE, signPdfResponseDTO);
+        logger.getLogger().debug("signPdf(" + signPdfRequestDTO + ") returning " + signPdfResponseDTO + ")", map);
 
-        return ResponseEntity.status(CREATED).body(response);
+        return ResponseEntity.status(CREATED).body(signPdfResponseDTO);
     }
 
 }
---
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/logging/LoggingUtils.java
@@ -0,0 +1,33 @@
+package uk.gov.companieshouse.documentsigningapi.logging;
+
+import uk.gov.companieshouse.logging.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoggingUtils {
+
+    public static final String SIGN_PDF_REQUEST = "sign_pdf_request";
+
+    public static final String SIGN_PDF_RESPONSE = "sign_pdf_response";
+
+    private final Logger logger;
+
+    public LoggingUtils(Logger logger) {
+        this.logger = logger;
+    }
+
+    public Map<String, Object> createLogMap() {
+        return new HashMap<>();
+    }
+
+    public void logIfNotNull(Map<String, Object> logMap, String key, Object loggingObject) {
+        if (loggingObject != null) {
+            logMap.put(key, loggingObject);
+        }
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+}
