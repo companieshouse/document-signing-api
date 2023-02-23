09bca1d925bd2235795af11946a8a252d68076fd
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-02-23 14:14:25 +0000

    DCAC-76: Fix broken test.

--- src/test/java/uk/gov/companieshouse/documentsigningapi/DocumentSigningApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/documentsigningapi/DocumentSigningApiApplicationTests.java
@@ -2,10 +2,16 @@ package uk.gov.companieshouse.documentsigningapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class DocumentSigningApiApplicationTests {
 
+	@MockBean
+	S3Client s3Client;
+
+	@SuppressWarnings("squid:S2699") // at least one assertion
 	@Test
 	void contextLoads() {
 	}
