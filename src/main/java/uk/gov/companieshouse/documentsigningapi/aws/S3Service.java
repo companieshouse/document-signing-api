2ce07fd6e5419e4374c717361b5a8eda6cefd884
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-03-01 16:36:13 +0000

    DCAC-94: Use optional folder prefix for signed document storage.

--- src/main/java/uk/gov/companieshouse/documentsigningapi/aws/S3Service.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/aws/S3Service.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.documentsigningapi.aws;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -12,13 +13,21 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import static org.apache.commons.lang.StringUtils.isEmpty;
+
 @Component
 public class S3Service {
 
+    private static final String DIRECTORY_SEPARATOR = "/";
+
     private final S3Client s3Client;
 
-    public S3Service(S3Client s3Client) {
+    private final String signedDocStoragePrefix;
+
+    public S3Service(S3Client s3Client,
+                     @Value("${environment.signed.doc.storage.prefix}") String signedDocStoragePrefix) {
         this.s3Client = s3Client;
+        this.signedDocStoragePrefix = signedDocStoragePrefix;
     }
 
     public ResponseInputStream<GetObjectResponse> retrieveUnsignedDocument(final String documentLocation)
@@ -35,15 +44,17 @@ public class S3Service {
     public String storeSignedDocument(final byte[] signedDocument) {
         final var bucketName = "document-signing-api";
         final var fileName = "signed-document.pdf";
+        final var filePath = isEmpty(signedDocStoragePrefix) ?
+                fileName : signedDocStoragePrefix + DIRECTORY_SEPARATOR + fileName;
         final var putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
-                .key(fileName)
+                .key(filePath)
                 .build();
         s3Client.putObject(putObjectRequest, RequestBody.fromBytes(signedDocument));
         final var s3Utilities = s3Client.utilities();
         final var getUrlRequest = GetUrlRequest.builder()
                 .bucket(bucketName)
-                .key(fileName)
+                .key(filePath)
                 .build();
         return s3Utilities.getUrl(getUrlRequest).toString();
     }
