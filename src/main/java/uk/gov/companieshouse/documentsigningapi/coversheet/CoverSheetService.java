da4bc91319ff368d6750cfbe36228af70dc618e9
caismith-ch <csmith2@companieshouse.gov.uk>
2023-03-10 13:31:02 +0000

    Refactor code to remove duplication

--- src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/CoverSheetService.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/CoverSheetService.java
@@ -20,6 +20,19 @@ public class CoverSheetService {
 
     private final LoggingUtils logger;
 
+    private static final float LEFT_MARGIN = 25;
+
+    private static final String PAGE_HEADING = "Companies House";
+    private static final String CERTIFIED_DOCUMENT_TYPE = "Certified document";
+    private static final String DOCUMENT_SIGNED_TEXT = "This file has been electronically signed by Companies House.";
+    private static final String PAGE_SPACER = "___________________________________________________";
+    private static final String VIEW_FILE_HEADING = "View this file and signature in Adobe Reader";
+    private static final String SIGNATURE_HELPTEXT = "This file has an embedded electronic signature " +
+        "that is only accessible in Adobe Reader. You can download Adobe Reader for free";
+    private static final String EMAIL_HELPTEXT = "This file can be emailed. You cannot make changes to this file.";
+    private static final String PRINTER_HELPTEXT = "This file is only valid in digital form as it contains a unique " +
+        "electronic signature. Printed copies of this file will not be accepted.";
+
     public CoverSheetService(LoggingUtils logger) {
         this.logger = logger;
     }
@@ -27,7 +40,7 @@ public class CoverSheetService {
     public byte[] addCoverSheet(final byte[] document) {
         try {
             final var pdfDocument = PDDocument.load(document);
-            insertBlankCoverSheet(pdfDocument);
+            insertCoverSheet(pdfDocument);
             return getContents(pdfDocument);
         } catch (IOException ioe) {
             logger.getLogger().error(ioe.getMessage(), ioe);
@@ -35,110 +48,63 @@ public class CoverSheetService {
         }
     }
 
-    private void insertBlankCoverSheet(final PDDocument pdfDocument) throws IOException {
-        final var blankPage = new PDPage(A4);
+    private void insertCoverSheet(final PDDocument pdfDocument) throws IOException {
+        final var coverSheet = new PDPage(A4);
+        buildCoverSheetContent(pdfDocument, coverSheet);
+        pdfDocument.getPages().insertBefore(coverSheet, pdfDocument.getPage(0));
+    }
 
+    private void buildCoverSheetContent(PDDocument pdfDocument, PDPage coverSheet) throws IOException {
+        //TODO currently hardcoded locations on docker container, will need to be updated
         PDImageXObject signatureImage = PDImageXObject.createFromFile("./app/resources/coversheet/signature.jpeg", pdfDocument);
         PDImageXObject emailImage = PDImageXObject.createFromFile("./app/resources/coversheet/email.jpeg", pdfDocument);
         PDImageXObject printerImage = PDImageXObject.createFromFile("./app/resources/coversheet/printer.jpeg", pdfDocument);
 
-        // Build the coversheet
-        PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, blankPage);
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 30);
-        contentStream.newLineAtOffset(25, 770);
-        contentStream.showText("Companies House");
-        contentStream.endText();
+        PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, coverSheet);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 18);
-        contentStream.newLineAtOffset(25, 750);
-        contentStream.showText("2nd November 2022");
-        contentStream.endText();
+        insertText(contentStream, PAGE_HEADING, PDType1Font.HELVETICA_BOLD, 30, LEFT_MARGIN, 770);
+        insertText(contentStream, "2nd November 2022", PDType1Font.HELVETICA, 18, LEFT_MARGIN, 750);
+        insertText(contentStream, CERTIFIED_DOCUMENT_TYPE, PDType1Font.HELVETICA_BOLD, 24, LEFT_MARGIN, 650);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 24);
-        contentStream.newLineAtOffset(25, 650);
-        contentStream.showText("Certified document");
-        contentStream.endText();
+        textWrapper(contentStream, "TEST COMPANY NAME (00000000)", 18, LEFT_MARGIN, 620);
+        textWrapper(contentStream, "Change of Registered Office Address (ADO1) ef", 18, 25, 590);
+        textWrapper(contentStream, DOCUMENT_SIGNED_TEXT, 18, LEFT_MARGIN, 560);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 18);
-        contentStream.newLineAtOffset(25, 620);
-        contentStream.showText("TEST COMPANY NAME (00000000)");
-        contentStream.endText();
+        insertText(contentStream, PAGE_SPACER, PDType1Font.HELVETICA, 18, LEFT_MARGIN, 530);
+        insertText(contentStream, VIEW_FILE_HEADING, PDType1Font.HELVETICA_BOLD, 18, LEFT_MARGIN, 480);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 18);
-        contentStream.newLineAtOffset(25, 590);
-        contentStream.showText("Change of Registered Office Address (ADO1) ef");
-        contentStream.endText();
+        contentStream.drawImage(signatureImage, LEFT_MARGIN, 420, 35, 35);
+        textWrapper(contentStream, SIGNATURE_HELPTEXT, 12, 70, 440);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 18);
-        contentStream.newLineAtOffset(25, 560);
-        contentStream.showText("This file has been electronically signed by Companies House.");
-        contentStream.endText();
+        contentStream.drawImage(emailImage, LEFT_MARGIN, 350, 35, 35);
+        textWrapper(contentStream, EMAIL_HELPTEXT, 12, 70, 370);
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 18);
-        contentStream.newLineAtOffset(25, 530);
-        contentStream.showText("____________________________________________________________________");
-        contentStream.endText();
-
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA_BOLD, 18);
-        contentStream.newLineAtOffset(25, 480);
-        contentStream.showText("View this file and signature in Adobe Reader");
-        contentStream.endText();
-
-        contentStream.drawImage(signatureImage, 25, 420, 35, 35);
-        String signatureHelpText = "This file has an embedded electronic signature that is only accessible in Adobe Reader. You can download Adobe Reader for free";
-        String[] signatureHelpTextLines = WordUtils.wrap(signatureHelpText, 80).split("\\r?\\n");
-        System.out.println("lines is of size : " + signatureHelpTextLines.length);
-
-        for(int i = 0; i < signatureHelpTextLines.length; i++){
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.newLineAtOffset(70,440-i*15);
-            contentStream.showText(signatureHelpTextLines[i]);
-            contentStream.newLine();
-            contentStream.endText();
-        }
-
-
-        contentStream.drawImage(emailImage, 25, 350, 35, 35);
-        String emailHelpText = "This file can be emailed. You cannot make changes to this file.";
-        String[] emailHelpTextLines = WordUtils.wrap(emailHelpText, 80).split("\\r?\\n");
-        System.out.println("lines is of size : " + emailHelpTextLines.length);
-
-        for(int i = 0; i < emailHelpTextLines.length; i++){
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.newLineAtOffset(70,370-i*15);
-            contentStream.showText(emailHelpTextLines[i]);
-            contentStream.newLine();
-            contentStream.endText();
-        }
-
-        contentStream.drawImage(printerImage, 25, 280, 35, 35);
-        String printerHelpText = "This file is only valid in digital form as it contains a unique electronic signature. Printed copies of this file will not be accepted.";
-        String[] printerHelpTextLines = WordUtils.wrap(signatureHelpText, 80).split("\\r?\\n");
-        System.out.println("lines is of size : " + printerHelpTextLines.length);
-
-        for(int i = 0; i < printerHelpTextLines.length; i++){
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA, 12);
-            contentStream.newLineAtOffset(70,300-i*15);
-            contentStream.showText(printerHelpTextLines[i]);
-            contentStream.newLine();
-            contentStream.endText();
-        }
+        contentStream.drawImage(printerImage, LEFT_MARGIN, 280, 35, 35);
+        textWrapper(contentStream, PRINTER_HELPTEXT, 12, 70, 300);
 
         contentStream.close();
+    }
 
+    private void insertText(PDPageContentStream contentStream, String text, PDType1Font font,
+                            float fontSize, float xPosition, float yPosition) throws IOException {
+        contentStream.beginText();
+        contentStream.setFont(font, fontSize);
+        contentStream.newLineAtOffset(xPosition, yPosition);
+        contentStream.showText(text);
+        contentStream.endText();
+    }
 
-        pdfDocument.getPages().insertBefore(blankPage, pdfDocument.getPage(0));
+    private void textWrapper(PDPageContentStream contentStream, String textToWrap, float fontSize, float xPosition, float yPosition) throws IOException {
+        String[] wrappedText = WordUtils.wrap(textToWrap, 80).split("\\r?\\n");
+
+        for(int i = 0; i < wrappedText.length; i++){
+            contentStream.beginText();
+            contentStream.setFont(PDType1Font.HELVETICA, fontSize);
+            contentStream.newLineAtOffset(xPosition,yPosition-i*15);
+            contentStream.showText(wrappedText[i]);
+            contentStream.newLine();
+            contentStream.endText();
+        }
     }
 
     private byte[] getContents(final PDDocument pdfDocument) throws IOException {
