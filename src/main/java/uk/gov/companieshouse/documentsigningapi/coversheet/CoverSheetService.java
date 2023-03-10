18dac5128e3a8d0345c3d7bebe9ccc6e5548930e
caismith-ch <csmith2@companieshouse.gov.uk>
2023-03-10 14:36:42 +0000

    Updates to page element positions

--- src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/CoverSheetService.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/CoverSheetService.java
@@ -20,18 +20,22 @@ public class CoverSheetService {
 
     private final LoggingUtils logger;
 
-    private static final float LEFT_MARGIN = 25;
+    // Coversheet measurements
+    private static final float DEFAULT_LEFT_MARGIN = 25;
+    private static final float INFORMATION_SECTION_IMAGE_HEIGHT = 25;
+    private static final float INFORMATION_SECTION_IMAGE_WIDTH = 25;
 
-    private static final String PAGE_HEADING = "Companies House";
+    // Coversheet Strings
     private static final String CERTIFIED_DOCUMENT_TYPE = "Certified document";
     private static final String DOCUMENT_SIGNED_TEXT = "This file has been electronically signed by Companies House.";
-    private static final String PAGE_SPACER = "___________________________________________________";
-    private static final String VIEW_FILE_HEADING = "View this file and signature in Adobe Reader";
-    private static final String SIGNATURE_HELPTEXT = "This file has an embedded electronic signature " +
-        "that is only accessible in Adobe Reader. You can download Adobe Reader for free";
     private static final String EMAIL_HELPTEXT = "This file can be emailed. You cannot make changes to this file.";
+    private static final String PAGE_HEADING = "Companies House";
+    private static final String PAGE_SPACER = "___________________________________________________";
     private static final String PRINTER_HELPTEXT = "This file is only valid in digital form as it contains a unique " +
         "electronic signature. Printed copies of this file will not be accepted.";
+    private static final String SIGNATURE_HELPTEXT = "This file has an embedded electronic signature " +
+        "that is only accessible in Adobe Reader. You can download Adobe Reader for free";
+    private static final String VIEW_FILE_HEADING = "View this file and signature in Adobe Reader";
 
     public CoverSheetService(LoggingUtils logger) {
         this.logger = logger;
@@ -55,41 +59,45 @@ public class CoverSheetService {
     }
 
     private void buildCoverSheetContent(PDDocument pdfDocument, PDPage coverSheet) throws IOException {
-        //TODO currently hardcoded locations on docker container, will need to be updated
+        //TODO CURRENTLY HARDCODED THE LOCATION OF THE IMAGES FOR DOCKER ENV, WILL NEED TO BE UPDATED
         PDImageXObject signatureImage = PDImageXObject.createFromFile("./app/resources/coversheet/signature.jpeg", pdfDocument);
         PDImageXObject emailImage = PDImageXObject.createFromFile("./app/resources/coversheet/email.jpeg", pdfDocument);
         PDImageXObject printerImage = PDImageXObject.createFromFile("./app/resources/coversheet/printer.jpeg", pdfDocument);
 
         PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, coverSheet);
 
-        insertText(contentStream, PAGE_HEADING, PDType1Font.HELVETICA_BOLD, 30, LEFT_MARGIN, 770);
-        insertText(contentStream, "2nd November 2022", PDType1Font.HELVETICA, 18, LEFT_MARGIN, 750);
-        insertText(contentStream, CERTIFIED_DOCUMENT_TYPE, PDType1Font.HELVETICA_BOLD, 24, LEFT_MARGIN, 650);
+        insertText(contentStream, PAGE_HEADING, PDType1Font.HELVETICA_BOLD, 30, 770);
 
-        textWrapper(contentStream, "TEST COMPANY NAME (00000000)", 18, LEFT_MARGIN, 620);
+        // TODO REPLACE WITH DATE OF SIGNATURE
+        insertText(contentStream, "2nd November 2022", PDType1Font.HELVETICA, 18, 750);
+        insertText(contentStream, CERTIFIED_DOCUMENT_TYPE, PDType1Font.HELVETICA_BOLD, 24, 650);
+
+        //TODO REPLACE WITH COMPANY NAME FROM REQUEST
+        textWrapper(contentStream, "TEST COMPANY NAME (00000000)", 18, DEFAULT_LEFT_MARGIN, 620);
+        //TODO REPLACE WITH DETAILS OF DOUCMENT BEING SIGNED
         textWrapper(contentStream, "Change of Registered Office Address (ADO1) ef", 18, 25, 590);
-        textWrapper(contentStream, DOCUMENT_SIGNED_TEXT, 18, LEFT_MARGIN, 560);
+        textWrapper(contentStream, DOCUMENT_SIGNED_TEXT, 18, DEFAULT_LEFT_MARGIN, 560);
 
-        insertText(contentStream, PAGE_SPACER, PDType1Font.HELVETICA, 18, LEFT_MARGIN, 530);
-        insertText(contentStream, VIEW_FILE_HEADING, PDType1Font.HELVETICA_BOLD, 18, LEFT_MARGIN, 480);
+        insertText(contentStream, PAGE_SPACER, PDType1Font.HELVETICA, 18, 530);
+        insertText(contentStream, VIEW_FILE_HEADING, PDType1Font.HELVETICA_BOLD, 18, 480);
 
-        contentStream.drawImage(signatureImage, LEFT_MARGIN, 420, 35, 35);
-        textWrapper(contentStream, SIGNATURE_HELPTEXT, 12, 70, 440);
+        contentStream.drawImage(signatureImage, DEFAULT_LEFT_MARGIN, 420, INFORMATION_SECTION_IMAGE_WIDTH, INFORMATION_SECTION_IMAGE_HEIGHT);
+        textWrapper(contentStream, SIGNATURE_HELPTEXT, 14, 70, 440);
 
-        contentStream.drawImage(emailImage, LEFT_MARGIN, 350, 35, 35);
-        textWrapper(contentStream, EMAIL_HELPTEXT, 12, 70, 370);
+        contentStream.drawImage(emailImage, DEFAULT_LEFT_MARGIN, 370, INFORMATION_SECTION_IMAGE_WIDTH, INFORMATION_SECTION_IMAGE_HEIGHT);
+        textWrapper(contentStream, EMAIL_HELPTEXT, 14, 70, 385);
 
-        contentStream.drawImage(printerImage, LEFT_MARGIN, 280, 35, 35);
-        textWrapper(contentStream, PRINTER_HELPTEXT, 12, 70, 300);
+        contentStream.drawImage(printerImage, DEFAULT_LEFT_MARGIN, 325, INFORMATION_SECTION_IMAGE_WIDTH, INFORMATION_SECTION_IMAGE_HEIGHT);
+        textWrapper(contentStream, PRINTER_HELPTEXT, 14, 70, 340);
 
         contentStream.close();
     }
 
     private void insertText(PDPageContentStream contentStream, String text, PDType1Font font,
-                            float fontSize, float xPosition, float yPosition) throws IOException {
+                            float fontSize, float yPosition) throws IOException {
         contentStream.beginText();
         contentStream.setFont(font, fontSize);
-        contentStream.newLineAtOffset(xPosition, yPosition);
+        contentStream.newLineAtOffset(DEFAULT_LEFT_MARGIN, yPosition);
         contentStream.showText(text);
         contentStream.endText();
     }
