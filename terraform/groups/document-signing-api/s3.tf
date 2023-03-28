6d1473739cd19ccc7a9fc68351091cc073dab73f
Jimmy <jtaylor@companieshouse.gov.uk>
2023-03-28 06:22:54 +0100

    Block public access

--- terraform/groups/document-signing-api/s3.tf
+++ terraform/groups/document-signing-api/s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket_acl" "signed" {
   bucket = aws_s3_bucket.signed.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_public_access_block" "signed" {
+  bucket = aws_s3_bucket.signed.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
