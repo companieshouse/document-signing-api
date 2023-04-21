1be8575efd47f6aacd7f33cc482a75c9cf1b1231
Marc Ransome <mransome@companieshouse.gov.uk>
2023-04-21 13:45:59 +0100

    Deprecate bucket ACL and replace with Object Ownership config

--- terraform/groups/document-signing-api/s3.tf
+++ terraform/groups/document-signing-api/s3.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "signed" {
   bucket = "${var.service}.${var.aws_account}.ch.gov.uk"
 }
 
-resource "aws_s3_bucket_acl" "signed" {
-  bucket = aws_s3_bucket.signed.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_public_access_block" "signed" {
   bucket = aws_s3_bucket.signed.id
 
@@ -14,4 +9,12 @@ resource "aws_s3_bucket_public_access_block" "signed" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "data" {
+  bucket = aws_s3_bucket.signed.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
