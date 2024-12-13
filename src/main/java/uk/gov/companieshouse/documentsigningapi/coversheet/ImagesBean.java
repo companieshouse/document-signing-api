d3cc2e2e3ed0b8e3038d2695a6661c7f31fdf9ad
dhawes-ch <dhawes@companieshouse.gov.uk>
2024-12-13 12:28:45 +0000

    rename var to better align with new approach

--- src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/ImagesBean.java
+++ src/main/java/uk/gov/companieshouse/documentsigningapi/coversheet/ImagesBean.java
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Component;
 public class ImagesBean {
 
     // Classpath mapping once compiled
-    private static final String DIRECTORY_SEPARATOR = "/coversheet/";
+    private static final String CLASSPATH_FOLDER = "/coversheet/";
 
     public PDImageXObject createImage(final String fileName, final PDDocument pdfDocument)
             throws IOException {
         final String filePath = Objects.requireNonNull(
-                getClass().getResource(DIRECTORY_SEPARATOR + fileName)).getPath();
+                getClass().getResource(CLASSPATH_FOLDER + fileName)).getPath();
         return PDImageXObject.createFromFile(filePath, pdfDocument);
     }
 }
