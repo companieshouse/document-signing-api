3700b6e3924663d77ff77fa31d155bbe2a53156b
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2025-01-08 15:37:27 +0000

    modifying lb and lb type from multi to single

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -34,23 +34,14 @@ module "ecs-service" {
   lb_listener_arn                 = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority       = local.lb_listener_rule_priority
   lb_listener_paths               = local.lb_listener_paths
-  multilb_setup                   = true
-  multilb_listeners               = {
-    "priv-api-lb": {
-      listener_arn                = data.aws_lb_listener.secondary_lb_listener.arn,
-      load_balancer_arn           = data.aws_lb.secondary_lb.arn
-    }
-    "pub-api-lb": {
-      load_balancer_arn      = data.aws_lb.service_lb.arn
-      listener_arn           = data.aws_lb_listener.service_lb_listener.arn
-    }
-  }
+  multilb_setup                   = false
 
   # ECS Task container health check
   use_task_container_healthcheck = true
   healthcheck_path               = local.healthcheck_path
   healthcheck_matcher            = local.healthcheck_matcher
   health_check_grace_period_seconds = 240
+  healthcheck_healthy_threshold     = "2"
 
   # Docker container details
   docker_registry   = var.docker_registry
@@ -75,7 +66,6 @@ module "ecs-service" {
 
   # Cloudwatch
   cloudwatch_alarms_enabled         = var.cloudwatch_alarms_enabled
-  multilb_cloudwatch_alarms_enabled = var.multilb_cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
   task_environment          = local.task_environment
